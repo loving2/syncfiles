d35ff78c46267e2e30ea5ad56e78e889d2def436
hogenshpogen <hogenshpogen@gmail.com>
2014-07-23 13:40:59 +0200

    Changed ~ to ${HOME}; fixes issue with cygwin.

--- install.sh
+++ install.sh
@@ -1,30 +1,30 @@
-mv ~/.vim ~/.vimbak$(date -d "today" +"%Y%m%d_%H%M")
-mkdir -p ~/.vim
-mkdir -p ~/.vim/colors && cd ~/.vim/colors && wget -O wombat256mod.vim http://www.vim.org/scripts/download_script.php?src_id=13400
-xrdb -merge ~/.Xresources
-mkdir -p ~/.vim/autoload ~/.vim/bundle
-#curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
-cd ~/.vim
+mv ${HOME}/.vim ${HOME}/.vimbak$(date -d "today" +"%Y%m%d_%H%M")
+mkdir -p ${HOME}/.vim
+mkdir -p ${HOME}/.vim/colors && cd ${HOME}/.vim/colors && wget -O wombat256mod.vim http://www.vim.org/scripts/download_script.php?src_id=13400
+xrdb -merge ${HOME}/.Xresources
+mkdir -p ${HOME}/.vim/autoload ${HOME}/.vim/bundle
+#curl -LSso ${HOME}/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+cd ${HOME}/.vim
 git clone https://github.com/tpope/vim-pathogen.git
-cd ~/.vim/autoload
+cd ${HOME}/.vim/autoload
 ln -s ../vim-pathogen/autoload/pathogen.vim ./pathogen.vim
-cd ~/.vim/vim-pathogen
+cd ${HOME}/.vim/vim-pathogen
 git checkout tags/v2.3
-cd ~/.vim/bundle
+cd ${HOME}/.vim/bundle
 git clone https://github.com/scrooloose/nerdtree.git
 git clone https://github.com/scrooloose/syntastic.git
 git clone git://github.com/tpope/vim-commentary.git
 git clone git://github.com/tpope/vim-surround.git
 git clone https://github.com/ervandew/supertab
 git clone https://github.com/majutsushi/tagbar.git
-cp ~/syncfiles/patches/tagbar.patch ~/.vim/bundle/tagbar/plugin/
-patch ~/.vim/bundle/tagbar/plugin/tagbar.vim -bnN -i ~/.vim/bundle/tagbar/plugin/tagbar.patch
-mkdir -p ~/.vim/plugin
+cp ${HOME}/syncfiles/patches/tagbar.patch ${HOME}/.vim/bundle/tagbar/plugin/
+patch ${HOME}/.vim/bundle/tagbar/plugin/tagbar.vim -bnN -i ${HOME}/.vim/bundle/tagbar/plugin/tagbar.patch
+mkdir -p ${HOME}/.vim/plugin
 wget http://www.vim.org/scripts/download_script.php?src_id=13834 -O toggle.vim
-cp toggle.vim ~/.vim/plugin && rm toggle.vim
+cp toggle.vim ${HOME}/.vim/plugin && rm toggle.vim
 
-rm -f ~/.bashrc && ln -s ~/syncfiles/dotfiles/bashrc ~/.bashrc
-rm -f ~/.screenrc && ln -s ~/syncfiles/dotfiles/screenrc ~/.screenrc
-rm -f ~/.vimrc && ln -s ~/syncfiles/dotfiles/vimrc ~/.vimrc
-rm -f ~/.Xresources && ln -s ~/syncfiles/dotfiles/Xresources ~/.Xresources
-rm -f ~/.tmux.conf && ln -s ~/syncfiles/dotfiles/tmuxrc ~/.tmux.conf
+rm -f ${HOME}/.bashrc && ln -s ${HOME}/syncfiles/dotfiles/bashrc ${HOME}/.bashrc
+rm -f ${HOME}/.screenrc && ln -s ${HOME}/syncfiles/dotfiles/screenrc ${HOME}/.screenrc
+rm -f ${HOME}/.vimrc && ln -s ${HOME}/syncfiles/dotfiles/vimrc ${HOME}/.vimrc
+rm -f ${HOME}/.Xresources && ln -s ${HOME}/syncfiles/dotfiles/Xresources ${HOME}/.Xresources
+rm -f ${HOME}/.tmux.conf && ln -s ${HOME}/syncfiles/dotfiles/tmuxrc ${HOME}/.tmux.conf
